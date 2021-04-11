83a6db3cdbc8535d07a9602257263d8897a227e3
GonzaloPeralCorrales <gptasis@yahoo.es>
2021-04-11 20:33:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {connect} from 'react-redux';
+import updateMovies from './action/updateMovies';
 
-function App() {
-  return (
+function App(props) {   
+  return (       
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+       <h3>REDUX MOVIELIST APP</h3>
+  <br/> 
 
-export default App;
+  <span style={{color:'green'}}>
+  YOUR CURRENT MOVIE IS: </span>{props.movies.name}
+
+  <br/>
+  <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+
+    </div>
+    
+);
+
+};
+
+const MapStateToProps = (state)=> {
+  
+      return {
+      
+      movies:state.movies
+  };
+};
+const MapDispatchToProps = (dispatch) => {
+
+  return{
+  
+  updateMovies: ()=> dispatch(updateMovies)
+
+}
+};
+
+export default connect(MapStateToProps, MapDispatchToProps)(App);
